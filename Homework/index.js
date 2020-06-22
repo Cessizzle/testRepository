a5e60dbc3cc56407d2096990e74556f09128897c
unknown <cmodest21@gmail.com>
2020-06-21 19:18:35 -0700

    change console log line

---
+++ Homework/index.js
@@ -0,0 +1,21 @@
+var itemPrice1 = 10.00;
+var itemPrice2 = 7.00;
+
+var walletMoney1 = 30.00;
+var walletMoney2 = 14.00;
+
+var numberOfFriends1 = 28;
+var numberOfFriends2 = 3;
+
+var ageInYears1 = 24;
+var ageInYears2 = 20;
+
+var firstName = "Chris"
+var lastName = "Redfield"
+var middleInitial = "M"
+
+console.log(walletMoney1 - itemPrice1);
+console.log(numberOfFriends1 / ageInYears1);
+console.log(firstName , middleInitial , lastName);
+
+
